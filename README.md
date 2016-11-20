8d5739d340a2f543e86b9a1020db12e05e03b02c
getweapp <yw@getweapp.com>
2016-11-20 16:42:20 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,29 @@
+# 微信小程序-dribbble
+
+### 说明：
+
+简单的dribbble客户端，目前只实现了首页的图片加载。
+
+亮点：1.下拉刷新、上拉加载；2.响应式布局，各个分辨率均能展现良好
+
+### 数据接口:
+
+- https://api.getweapp.com/thirdparty/dribbble/shots
+
+### 目录结构：
+
+- image — 存放项目底部tab图片
+- pages — 存放项目页面相关文件，一共包括index,logs等几个页面
+- utils — 存放数据获取api和日期格式化等文件
+
+### 开发环境：
+
+微信web开发者工具 v0.10.102800
+
+### 项目截图：
+
+https://www.getweapp.com/project?projectId=5831615213a0fe03286b616e
+
+### 感谢:
+
+本项目原始版本由sulihuang提供：https://github.com/sulihuang/wechat-dribbble
---
+++ pages/index/index.js
@@ -0,0 +1,50 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const utils = require('../../utils/util.js')
+const api = require('../../utils/api.js')
+
+Page({
+  data: {
+    list: [],
+    loading: false,
+    windowHeight: 0,
+    windowWidth: 0
+  },
+  onLoad: function () {
+    this.setData({
+      loading: false
+    })
+    app.fetchApi(api.getShots2(), (err, res) => {
+
+      //更新数据
+      this.setData({list: res, loading: true})
+    })
+    this.index = 1
+  },
+
+  onShow: function(e) {
+    wx.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          windowHeight: res.windowHeight,
+          windowWidth: res.windowWidth
+        })
+      }
+    })
+  },
+
+  pullDownRefresh: function(e) {
+    // 下拉刷新，重新加载数据； 
+    // TODO，不重新加载，只更新对应列表；
+    this.onLoad()
+  },
+
+  pullUpLoad: function (e) {
+    if (this.data.list.length === 0) return;
+    app.fetchApi(api.getShots2({page: ++this.index}), (err, res) => {
+      //更新数据
+      this.setData({list: this.data.list.concat(res)})
+    })
+  },
+})
